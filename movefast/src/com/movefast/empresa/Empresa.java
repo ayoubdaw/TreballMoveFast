c3debb31d5baf7591bddcb3cb2fc9a3c4c3319a4
ayoub <ayoubbouyssfdaouan@iespaucasesnoves.cat>
2018-05-16 17:49:19 +0200

    Getters , setters , constructors

--- movefast/src/com/movefast/empresa/Empresa.java
+++ movefast/src/com/movefast/empresa/Empresa.java
@@ -5,10 +5,40 @@
  */
 package com.movefast.empresa;
 
-
 public class Empresa {
-    
+
+    private boolean descompte;
     private int cif;
-           
-    
+    private String nomEmpresa;
+
+    public Empresa(boolean descompte, int cif, String nomEmpresa) {
+        this.descompte = descompte;
+        this.cif = cif;
+        this.nomEmpresa = nomEmpresa;
+    }
+
+    public boolean isDescompte() {
+        return descompte;
+    }
+
+    public void setDescompte(boolean descompte) {
+        this.descompte = descompte;
+    }
+
+    public int getCif() {
+        return cif;
+    }
+
+    public void setCif(int cif) {
+        this.cif = cif;
+    }
+
+    public String getNomEmpresa() {
+        return nomEmpresa;
+    }
+
+    public void setNomEmpresa(String nomEmpresa) {
+        this.nomEmpresa = nomEmpresa;
+    }
+
 }
