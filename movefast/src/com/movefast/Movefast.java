a26300345a75d0cfb8c4539a1072295e775db77b
ayoub <ayoubbouyssfdaouan@iespaucasesnoves.cat>
2018-05-16 20:43:21 +0200

    Esteve ,Emilio , Ayoub junt.

--- movefast/src/com/movefast/Movefast.java
+++ movefast/src/com/movefast/Movefast.java
@@ -2,42 +2,49 @@ package com.movefast;
 
 import com.movefast.clients.Client;
 import com.movefast.empresa.Empresa;
+import com.movefast.vehicles.Vehicle;
 import java.util.HashMap;
 
-
 public class Movefast {
-   private HashMap<String, Client> clients;
-   private HashMap<String, Empresa> empreses;
-   
+
+    private HashMap<String, Client> clients;
+    private HashMap<String, Empresa> empreses;
+    private HashMap<String, Vehicle> vehicles;
+
     public Movefast() {
         clients = new HashMap<String, Client>();
         empreses = new HashMap<String, Empresa>();
+        vehicles = new HashMap<String, Vehicle>();
     }
 
- 
+    public void altaClient(Client client) {
 
-    public void altaClient( Client client) {
-        
         clients.put(client.getDni(), client);
-        
+
     }
-    
-    public void baixaClient(String dni){
+
+    public void baixaClient(String dni) {
         clients.remove(dni);
-        
+
     }
-    
-    public Client cercarClient(String dni){
+
+    public Client cercarClient(String dni) {
         return clients.get(dni);
-        
+
     }
+
     public HashMap<String, Client> getClients() {
         return clients;
     }
-    
-     public void altaEmpresa(Empresa empresa) {
+
+    public void altaEmpresa(Empresa empresa) {
 
         empreses.put(empresa.getCif(), empresa);
+
+    }
+
+    public void altaVehicle(Vehicle vehicle) {
+        vehicles.put(vehicle.getMatricula(), vehicle);
 
     }
 
--- movefast/src/com/movefast/proves/Prova.java
+++ movefast/src/com/movefast/proves/Prova.java
@@ -1,29 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.movefast.proves;
 import com.movefast.Movefast;
 import com.movefast.clients.Client;
+import com.movefast.vehicles.Vehicle;
 import java.util.HashMap;
 
 public class Prova {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         Movefast c = new Movefast();
         Client Ayoub = new Client("46391111L", "Ayoub", "Bouyssfdaouan", 601226553, "458788741p");
         Client Emilio = new Client("463911231L", "Emilio", "Gallardo", 601226553, "4587f21741p");
-        
+        Vehicle Vehicle1 = new Vehicle("7458-QWE", 105.5, "B", true);
+        Vehicle Vehicle2 = new Vehicle("1558-TKR", 105.5, "B", true);
+
         c.altaClient( Ayoub);
         c.altaClient(Emilio);
         HashMap<String, Client> clients = c.getClients();
         c.baixaClient("46391111L");
        // System.out.println(clients);
        c.cercarClient("463911231L");
+       c.altaVehicle(Vehicle2);
+        
+        
+
     }
     
 }
