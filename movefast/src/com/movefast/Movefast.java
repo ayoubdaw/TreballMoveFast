840074a73e31cbf54d90e277ade75b38b7b23769
ayoub <ayoubbouyssfdaouan@iespaucasesnoves.cat>
2018-05-17 15:00:10 +0200

    Afegit metodes.

--- movefast/src/com/movefast/Movefast.java
+++ movefast/src/com/movefast/Movefast.java
@@ -6,46 +6,70 @@ import com.movefast.vehicles.Vehicle;
 import java.util.HashMap;
 
 public class Movefast {
-
+    
     private HashMap<String, Client> clients;
     private HashMap<String, Empresa> empreses;
     private HashMap<String, Vehicle> vehicles;
-
+    
     public Movefast() {
         clients = new HashMap<String, Client>();
         empreses = new HashMap<String, Empresa>();
         vehicles = new HashMap<String, Vehicle>();
     }
-
+    
     public void altaClient(Client client) {
-
+        
         clients.put(client.getDni(), client);
-
+        
     }
-
+    
     public void baixaClient(String dni) {
         clients.remove(dni);
-
+        
     }
-
+    
     public Client cercarClient(String dni) {
         return clients.get(dni);
-
+        
     }
-
+    
     public HashMap<String, Client> getClients() {
         return clients;
     }
-
+    
     public void altaEmpresa(Empresa empresa) {
-
+        
         empreses.put(empresa.getCif(), empresa);
-
+        
     }
-
+    
+    public void baixaEmpresa(String cif) {
+        empreses.remove(cif);
+        
+    }
+    
+    public Empresa cercarEmpresa(String cif) {
+        return empreses.get(cif);
+    }
+    
     public void altaVehicle(Vehicle vehicle) {
         vehicles.put(vehicle.getMatricula(), vehicle);
-
+        
     }
-
+    
+    public void baixaVehicle(String matricula) {
+        vehicles.remove(matricula);
+    }
+    
+    public Vehicle cercarVehicle(String matricula) {
+        return vehicles.get(matricula);
+        
+    }
+    
+    public HashMap<String, Vehicle> getVehicles() {
+        return vehicles;
+        
+    }
+    
+    
 }
--- movefast/src/com/movefast/proves/Prova.java
+++ movefast/src/com/movefast/proves/Prova.java
@@ -1,4 +1,5 @@
 package com.movefast.proves;
+
 import com.movefast.Movefast;
 import com.movefast.clients.Client;
 import com.movefast.vehicles.Vehicle;
@@ -6,24 +7,28 @@ import java.util.HashMap;
 
 public class Prova {
 
-    
     public static void main(String[] args) {
         Movefast c = new Movefast();
         Client Ayoub = new Client("46391111L", "Ayoub", "Bouyssfdaouan", 601226553, "458788741p");
         Client Emilio = new Client("463911231L", "Emilio", "Gallardo", 601226553, "4587f21741p");
         Vehicle Vehicle1 = new Vehicle("7458-QWE", 105.5, "B", true);
-        Vehicle Vehicle2 = new Vehicle("1558-TKR", 105.5, "B", true);
+        Vehicle Vehicle2 = new Vehicle("1558-TKR", 105.5, "B", false);
 
-        c.altaClient( Ayoub);
+        c.altaClient(Ayoub);
         c.altaClient(Emilio);
         HashMap<String, Client> clients = c.getClients();
+        HashMap<String, Vehicle> vehicles = c.getVehicles();
         c.baixaClient("46391111L");
-       // System.out.println(clients);
-       c.cercarClient("463911231L");
-       c.altaVehicle(Vehicle2);
-        
-        
+        // System.out.println(clients);
+        // System.out.println(c.cercarClient("463911231L"));
+        c.altaVehicle(Vehicle1);
+        c.altaVehicle(Vehicle2);
+        //System.out.println(vehicles);
+        // c.baixaVehicle("7458-QWE");
+        // System.out.println(vehicles);
+        System.out.println(c.cercarVehicle("1558-TKR"));
+        // System.out.println(vehicles);
 
     }
-    
+
 }
