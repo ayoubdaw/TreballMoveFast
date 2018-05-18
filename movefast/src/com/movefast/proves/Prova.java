a459fd472da5a91952412208d0b0b6d7e5ec2675
emiliogallardo <emiliogallardo@iespaucasesnoves.cat>
2018-05-18 19:00:25 +0200

    canvis

--- movefast/src/com/movefast/proves/Prova.java
+++ movefast/src/com/movefast/proves/Prova.java
@@ -8,10 +8,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
-
 public class Prova {
-
+    
     public static void main(String[] args) {
         Movefast c = new Movefast();
         Client Ayoub = new Client("46391111L", "Ayoub", "Bouyssfdaouan", 601226553, "458788741p");
@@ -20,15 +18,15 @@ public class Prova {
         Vehicle Vehicle1 = new Vehicle("7458-QWE", 105.5, "B", true);
         Vehicle Vehicle2 = new Vehicle("1558-TKR", 105.5, "B", false);
         
-        LocalDate date1 = LocalDate.of(2018,5,15);
-        LocalDate date2 = LocalDate.of(2018,6,15);
-        LocalDate dataLliurament = LocalDate.of(2018,5,17);
-        LocalDate dataEntrega = LocalDate.of(2018,6,10);
-
+        LocalDate date1 = LocalDate.of(2018, 5, 15);
+        LocalDate date2 = LocalDate.of(2018, 6, 15);
+        LocalDate dataLliurament = LocalDate.of(2018, 5, 17);
+        LocalDate dataEntrega = LocalDate.of(2018, 6, 10);
         
-        Lloguer llog1 = new Lloguer(1,dataLliurament,dataEntrega,Vehicle1,Emilio);
-        Lloguer llog2 = new Lloguer(2,date1,date2,Vehicle2,Emilio);
-
+       
+        Lloguer llog1 = new Lloguer(1, dataLliurament, dataEntrega,Vehicle1,Emilio);
+        Lloguer llog2 = new Lloguer(2, date1, date2,Vehicle2,Emilio);
+        
         c.altaClient(Ayoub);
         c.altaClient(Emilio);
         HashMap<String, Client> clients = c.getClients();
@@ -41,14 +39,17 @@ public class Prova {
         //System.out.println(vehicles);
         // c.baixaVehicle("7458-QWE");
         // System.out.println(vehicles);
-        System.out.println(c.cercarVehicle("1558-TKR"));
+       // System.out.println(c.cercarVehicle("1558-TKR"));
         // System.out.println(vehicles);
 
         c.omplirLloguers(llog1);
         c.omplirLloguers(llog2);
         //System.out.println(c.mostrarLloguers(llog1));
-        c.mostrarLloguers();
-
+        ArrayList<Lloguer> mostrarLloguers = c.mostrarLloguers(Emilio);
+        for (Lloguer actual : mostrarLloguers) {
+            System.out.println(actual.toString());
+        }
+        
     }
-
+    
 }
