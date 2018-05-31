e4ba9c43b21515c4926eeef132fdb4a4b876dc32
ayoub <ayoub@Ayoub-Bony>
2018-05-31 18:38:45 +0200

    throws exeptions

--- movefast/src/com/movefast/lloguer/Lloguer.java
+++ movefast/src/com/movefast/lloguer/Lloguer.java
@@ -1,6 +1,7 @@
 package com.movefast.lloguer;
 
 import com.movefast.clients.Client;
+import com.movefast.excepcions.LloguerException;
 import com.movefast.vehicles.Vehicle;
 import java.time.Duration;
 import java.time.LocalDate;
@@ -14,7 +15,7 @@ public class Lloguer {
     private Vehicle vehicle;
     private Client client;
 
-    public Lloguer(int numFactura, LocalDate dateLliurament, LocalDate dateEntrega, Vehicle vehicle, Client client) {
+    public Lloguer(int numFactura, LocalDate dateLliurament, LocalDate dateEntrega, Vehicle vehicle, Client client) throws LloguerException{
         this.numFactura = numFactura;
         this.dateLliurament = dateLliurament;
         this.dateEntrega = dateEntrega;
@@ -26,7 +27,10 @@ public class Lloguer {
         return numFactura;
     }
 
-    public void setNumFactura(int numFactura) {
+    public void setNumFactura(int numFactura) throws LloguerException {
+        if (numFactura == 0) {
+            throw new LloguerException ("El numero de factura no pot esser 0 o null");
+        }
         this.numFactura = numFactura;
     }
 
@@ -34,7 +38,10 @@ public class Lloguer {
         return dateLliurament;
     }
 
-    public void setDateLliurament(LocalDate dateLliurament) {
+    public void setDateLliurament(LocalDate dateLliurament) throws LloguerException {
+        if (dateLliurament.isBefore(LocalDate.now()) || dateLliurament.isEqual(LocalDate.now())) {
+        throw new LloguerException ("Data de lliurament no pot esser anterior a avui, ni avui");
+    }        
         this.dateLliurament = dateLliurament;
     }
 
@@ -42,7 +49,10 @@ public class Lloguer {
         return dateEntrega;
     }
 
-    public void setDateEntrega(LocalDate dateEntrega) {
+    public void setDateEntrega(LocalDate dateEntrega) throws LloguerException {
+        if (dateEntrega.isBefore(dateLliurament)) {
+            throw new LloguerException ("Data entrega no pot esser anterior a la data lliurament");
+        }
         this.dateEntrega = dateEntrega;
     }
 
@@ -50,7 +60,10 @@ public class Lloguer {
         return vehicle;
     }
 
-    public void setVehicle(Vehicle vehicle) {
+    public void setVehicle(Vehicle vehicle) throws LloguerException {
+        if (vehicle == null) {
+            throw new LloguerException("El vehicle no potser null");
+        }
         this.vehicle = vehicle;
     }
 
@@ -58,7 +71,10 @@ public class Lloguer {
         return client;
     }
 
-    public void setClient(Client client) {
+    public void setClient(Client client) throws LloguerException{
+        if (vehicle == null) {
+            throw new LloguerException("El client no pot esser null");
+        }
         this.client = client;
     }
 
